43724385742c86f6bcc660debc891fbee68dc159
Gard Sandholt <gard.sandholt@gmail.com>
2014-09-02 13:36:42 +0200

    Testing set up

---
+++ library/src/test/java/com/chenso/lib/ChensoXMLDocumentTest.java
@@ -0,0 +1,14 @@
+package com.chenso.lib;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChensoXMLDocumentTest {
+
+	@Test
+	public void testTest() {
+		assertEquals("This is chenso", "This is chenso");
+	}
+
+}
