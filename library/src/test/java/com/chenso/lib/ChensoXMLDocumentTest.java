759a72e00f1ff9b1009ba92179216e6d81cd5b93
Gard Sandholt <gard.sandholt@gmail.com>
2014-09-02 13:50:20 +0200

    Added tests

--- library/src/test/java/com/chenso/lib/ChensoXMLDocumentTest.java
+++ library/src/test/java/com/chenso/lib/ChensoXMLDocumentTest.java
@@ -6,9 +6,22 @@ import org.junit.Test;
 
 public class ChensoXMLDocumentTest {
 
+	private final String xmlString = "<note><to type=\"firstname\">Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>";
+
 	@Test
-	public void testTest() {
-		assertEquals("This is chenso", "This is chenso");
+	public void testFirstValueForNodeName() {
+		ChensoXMLDocument document = ChensoXMLDocument.XMLDocumentWithXMLString(xmlString);
+
+		assertEquals("Tove", document.firstValueForNodeName("to"));
+		assertEquals("Jani", document.firstValueForNodeName("from"));
+		assertEquals("Reminder", document.firstValueForNodeName("heading"));
+		assertEquals("Don't forget me this weekend!", document.firstValueForNodeName("body"));
 	}
 
+	@Test
+	public void testFirstValueForAttributeName() {
+		ChensoXMLDocument document = ChensoXMLDocument.XMLDocumentWithXMLString(xmlString);
+
+		assertEquals("firstname", document.firstValueForAttributeName("type"));
+	}
 }
